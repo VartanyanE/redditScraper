928764408f9aff7bf725cb4bad93456436aa0a8d
VartanyanE <mike.vartanyan@yahoo.com>
2022-08-13 20:57:59 -0700

    data on page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
 import Button from "@mui/material/Button";
+import { useState } from "react";
 
 function App() {
+  const [payload, setPayload] = useState(0);
   const data = () => {
     fetch("http://localhost:8000/finance/wallstreetbets")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setPayload(data));
   };
+  console.log(payload);
   return (
     <div className="App">
       <Button variant="contained" onClick={data}>
         Text
       </Button>
+      <br />
+      {payload ? payload.map((item) => <li>{item.title}</li>) : " "}
     </div>
   );
 }
