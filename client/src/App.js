185745ae3275af46a0034245f02681948d3565a6
VartanyanE <mike.vartanyan@yahoo.com>
2022-08-16 21:41:05 -0700

    images

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import image from "./assets/reddit-logo.png";
+import Cnbc from "./assets/cnbc.png";
 
 function App() {
   const [payload, setPayload] = useState(0);
@@ -25,61 +27,63 @@ function App() {
   console.log(payload);
 
   return (
-    <div className="App">
-      <Button variant="contained" onClick={redditData}>
-        Reddit
-      </Button>
-      <Button variant="contained" onClick={cnbcData}>
-        CNBC
-      </Button>
-      <br />
+    <div className="container">
+      <div className="card">
+        <div className="reddit-image">
+          <img className="reddit" src={image} onClick={redditData} />
+        </div>
+        <div className="cnbc-image">
+          <img className="cnbc" src={Cnbc} onClick={cnbcData} />
+        </div>
+        <br />
 
-      {payload
-        ? payload.map((item) => (
-            <>
-              <Card variant="outlined" sx={{ maxWidth: 275 }}>
-                <CardContent>
-                  <Typography
-                    sx={{ fontSize: 10 }}
-                    color="text.secondary"
-                    gutterBottom
-                  ></Typography>
-                  <Typography variant="h7" component="div">
-                    <a href={item.url}>{item.title}</a>{" "}
-                  </Typography>
-                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    r/{item.subreddit}{" "}
-                  </Typography>
-                  <Typography variant="body2"></Typography>
-                </CardContent>
-              </Card>
-            </>
-          ))
-        : " "}
+        {payload
+          ? payload.map((item) => (
+              <>
+                <Card variant="outlined" sx={{ maxWidth: 275 }}>
+                  <CardContent>
+                    <Typography
+                      sx={{ fontSize: 10 }}
+                      color="text.secondary"
+                      gutterBottom
+                    ></Typography>
+                    <Typography variant="h7" component="div">
+                      <a href={item.url}>{item.title}</a>{" "}
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      r/{item.subreddit}{" "}
+                    </Typography>
+                    <Typography variant="body2"></Typography>
+                  </CardContent>
+                </Card>
+              </>
+            ))
+          : " "}
 
-      {cnbc
-        ? cnbc.map((item) => (
-            <>
-              <Card variant="outlined" sx={{ maxWidth: 275 }}>
-                <CardContent>
-                  <Typography
-                    sx={{ fontSize: 10 }}
-                    color="text.secondary"
-                    gutterBottom
-                  ></Typography>
-                  <Typography variant="h7" component="div">
-                    <a href={item.url}>{item.title}</a>{" "}
-                  </Typography>
-                  <Typography
-                    sx={{ mb: 1.5 }}
-                    color="text.secondary"
-                  ></Typography>
-                  <Typography variant="body2"></Typography>
-                </CardContent>
-              </Card>
-            </>
-          ))
-        : " "}
+        {cnbc
+          ? cnbc.map((item) => (
+              <>
+                <Card variant="outlined" sx={{ maxWidth: 275 }}>
+                  <CardContent>
+                    <Typography
+                      sx={{ fontSize: 10 }}
+                      color="text.secondary"
+                      gutterBottom
+                    ></Typography>
+                    <Typography variant="h7" component="div">
+                      <a href={item.url}>{item.title}</a>{" "}
+                    </Typography>
+                    <Typography
+                      sx={{ mb: 1.5 }}
+                      color="text.secondary"
+                    ></Typography>
+                    <Typography variant="body2"></Typography>
+                  </CardContent>
+                </Card>
+              </>
+            ))
+          : " "}
+      </div>
     </div>
   );
 }
