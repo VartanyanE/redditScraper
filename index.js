47e7e4f4acb8c5bdbb1ce5231e835bbc4165bd56
Emanuil Vartanyan <emanuilvartanyan@Emanuils-MBP.lan>
2022-08-28 18:55:10 -0700

    refactor

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import cors from "cors";
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.use(cors());
 const codingRoutes = [
