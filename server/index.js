75869ceb798816dcfd9fe49c7e4657010e737f58
Emanuil Vartanyan <emanuilvartanyan@Emanuils-MBP.lan>
2022-08-28 18:44:14 -0700

    added path

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import cors from "cors";
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 app.use(cors());
 const codingRoutes = [
   {
